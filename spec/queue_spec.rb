cbe71a4ca317358e334e97a6c091d432fca9e432
Jacek Brożek <jac33k@gmail.com>
2014-07-14 20:04:18 +0200

    test for single character argument

--- spec/queue_spec.rb
+++ spec/queue_spec.rb
@@ -5,13 +5,16 @@ describe Queue do
   describe '.parse' do
 
     context 'list of jobs given' do
-
       it 'returns separated attributes' do
         expect(Queue.parse("a b c")).to eq ['a', 'b', 'c']
       end
-
     end
 
+    context 'single job given' do
+      it 'returns one-element collection' do
+        expect(Queue.parse("a")).to eq ['a']
+      end
+    end
 
   end
 end
