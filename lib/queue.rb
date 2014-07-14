466cf2fb673d447c990f2ea2268a6ceb97831860
Jacek Brożek <jac33k@gmail.com>
2014-07-14 23:55:20 +0200

    start using Job class

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,27 +1,13 @@
-require 'pry'
+require "job"
 
 class Queue
 
-  attr_reader :jobs
-
   def initialize(input)
-    @jobs = parse(input)
-    sanitize!
+    @jobs = Job.build(input)
   end
 
-  private
-
-  def parse(input)
-    input = input.split("\n")
-    input.map! do |j|
-      a = j.split('=>')
-      a.map(&:strip)
-    end
-  end
-
-  def sanitize!
-    @jobs.flatten!
-    @jobs -= [nil, ""]
+  def jobs
+    @jobs.map(&:to_s)
   end
 
 end
