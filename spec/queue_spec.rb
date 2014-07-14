9eb41b67a98bb9ebe0793ae810aa273f5768b22a
Jacek Brożek <jac33k@gmail.com>
2014-07-14 20:00:35 +0200

    add context

--- spec/queue_spec.rb
+++ spec/queue_spec.rb
@@ -2,8 +2,16 @@ require 'queue'
 
 describe Queue do
 
-  it 'returns separated attributes' do
-    expect(Queue.parse("a b c")).to eq ['a', 'b', 'c']
-  end
+  describe '.parse' do
 
+    context 'list of jobs given' do
+
+      it 'returns separated attributes' do
+        expect(Queue.parse("a b c")).to eq ['a', 'b', 'c']
+      end
+
+    end
+
+
+  end
 end
