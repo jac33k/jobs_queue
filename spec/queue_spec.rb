5616feef6e98a01411dad338cb663e2176ced741
Jacek Brożek <jac33k@gmail.com>
2014-07-14 23:07:50 +0200

    start refactoring

--- spec/queue_spec.rb
+++ spec/queue_spec.rb
@@ -6,21 +6,26 @@ describe Queue do
 
     context 'list of jobs given' do
       let(:input) { File.read('spec/fixtures/multiple_arguments.txt') }
+      subject { Queue.new(input).jobs }
 
       it 'returns separated attributes' do
-        expect(Queue.parse(input)).to eq ['a', 'b', 'c']
+        expect(subject).to eq ['a', 'b', 'c']
       end
     end
 
     context 'single job given' do
+      subject { Queue.new("a => ").jobs }
+
       it 'returns one-element collection' do
-        expect(Queue.parse("a => ")).to eq ['a']
+        expect(subject).to eq ['a']
       end
     end
 
     context 'blank list given' do
+      subject { Queue.new("").jobs }
+
       it 'returns blank collection' do
-        expect(Queue.parse("")).to eq []
+        expect(subject).to eq []
       end
     end
 
