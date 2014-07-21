cbfe51caabd775ae1b93e4977239d40fd31b5a1c
Jacek Brożek <jac33k@gmail.com>
2014-07-21 10:40:26 +0200

    small ref

--- lib/collection.rb
+++ lib/collection.rb
@@ -4,16 +4,15 @@ class Collection < Array
     sorted = []
 
     self.each do |job|
+      parent = self.find_by(job.parent)
       if sorted.include?(job)
-        parent = self.find_by(job.parent)
         index = sorted.index(job)
         sorted.insert(index, parent) unless parent.nil?
-      elsif !sorted.find {|j| j.name == job.parent}.nil?
-        parent = self.find_by(job.parent)
+      elsif sorted.include?(parent)
         index = sorted.index(parent)
         sorted.insert(index + 1, job)
       else
-        sorted << self.find_by(job.parent) if job.has_parent?
+        sorted << parent if job.has_parent?
         sorted << job
       end
     end
