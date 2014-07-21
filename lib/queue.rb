f7d4dc2c895fb2b7c721318dab5f54cadd4296c4
Jacek Brożek <jac33k@gmail.com>
2014-07-21 04:06:32 +0200

    add validations

--- lib/queue.rb
+++ lib/queue.rb
@@ -5,6 +5,7 @@ class Queue
   def initialize(input)
     @jobs = Job.build(input)
     sort!
+    validate
   end
 
   def jobs
@@ -31,5 +32,32 @@ class Queue
       end
     end
     @jobs = sorted
+  end
+
+  def validate
+    validate_circular_dependencies
+    validate_self_dependencies
+  end
+
+  def validate_circular_dependencies
+    visited = []
+    copy = @jobs.dup
+    while job = copy.shift
+      succ = job
+      while !succ.nil?
+        visited << succ
+        succ = @jobs.find{|j| j.name == succ.parent}
+        unless visited.detect{ |e| visited.count(e) > 1 }.nil?
+          raise "Jobs can't have circular dependencies."
+        end
+      end
+      visited = []
+    end
+  end
+
+  def validate_self_dependencies
+    if @jobs.any?{|j| j.name == j.parent}
+      raise "Jobs can't depend on themselves"
+    end
   end
 end
