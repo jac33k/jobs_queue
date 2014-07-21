aaf3ddf945a8802eaca519025e2bf770b83d837c
Jacek Brożek <jac33k@gmail.com>
2014-07-21 04:06:12 +0200

    add sorting method

--- lib/queue.rb
+++ lib/queue.rb
@@ -4,10 +4,32 @@ class Queue
 
   def initialize(input)
     @jobs = Job.build(input)
+    sort!
   end
 
   def jobs
     @jobs.map(&:to_s)
   end
 
+  private
+
+  def sort!
+    sorted = []
+
+    @jobs.each do |job|
+      if !sorted.find {|j| j.name == job.name}.nil?
+        parent = @jobs.find {|j| j.name == job.parent}
+        index = sorted.index(job)
+        sorted.insert(index, parent) unless parent.nil?
+      elsif !sorted.find {|j| j.name == job.parent}.nil?
+        parent = @jobs.find {|j| j.name == job.parent}
+        index = sorted.index(parent)
+        sorted.insert(index + 1, job)
+      else
+        sorted << @jobs.find{|j| j.name == job.parent } if job.has_parent?
+        sorted << job
+      end
+    end
+    @jobs = sorted
+  end
 end
