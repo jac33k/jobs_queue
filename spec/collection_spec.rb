ec806c3c356e406dc02b9be3a37785b633b2dea4
Jacek Brożek <jac33k@gmail.com>
2014-07-21 10:28:08 +0200

    perform sort! method on collection, spec

--- spec/collection_spec.rb
+++ spec/collection_spec.rb
@@ -9,6 +9,18 @@ describe Collection do
     it 'returns proper job' do
       expect(list.find_by('a')).to eq job1
     end
+  end
 
+  describe '#sort!' do
+    let(:job1) { Job.new('a') }
+    let(:job2) { Job.new('b', 'c') }
+    let(:job3) { Job.new('c') }
+
+    subject { Collection.new([job1, job2, job3]).sort! }
+
+    it 'returns sorted list' do
+      expect(subject.index(job3)).to be < subject.index(job2)
+      expect(subject.uniq.size).to eq 3
+    end
   end
 end
